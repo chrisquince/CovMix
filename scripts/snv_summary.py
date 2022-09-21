9116c2519af78cffe822f621a17a1567b57fdbd0
Sebastien-Raguideau <idaho90@live.fr>
2022-09-21 13:34:20 +0100

    tweaked plot so that we use only the varscan positions

--- scripts/snv_summary.py
+++ scripts/snv_summary.py
@@ -4,6 +4,7 @@ import argparse
 import numpy as np
 from os.path import basename
 import matplotlib.pyplot as plt
+from operator import itemgetter
 import matplotlib.backends.backend_pdf
 from collections import defaultdict,Counter
 from matplotlib.colors import ListedColormap
@@ -11,13 +12,28 @@ from Bio.SeqIO.FastaIO import SimpleFastaParser as sfp
 # plot counts predicted vs observed and write that info in a file
 # let's create a file with pos,base,percent
 
+
+def union(intervals):
+    sorted_intervals = sorted([tuple(sorted(i)) for i in intervals], key=itemgetter(0))
+    if len(sorted_intervals)<=1:  # no intervals to merge
+        return
+    # low and high represent the bounds of the current run of merges
+    low, high = sorted_intervals[0]
+    for iv in sorted_intervals[1:]:
+        if iv[0] <= high:  # new interval overlaps current run
+            high = max(high, iv[1])  # merge with the current run
+        else:  # current run is over
+            yield low, high  # yield accumulated interval
+            low, high = iv  # start new run
+    yield low, high  # end the final run
+
+
+
+
 # try another approach with completely unfiltered output
-def unfiltered_proportions(ROOT):
+def unfiltered_proportions(ROOT,sorted_refs,sorted_samples):
     files = glob.glob("%s/samples/*/EM_runs/*_total_pi_est.csv"%ROOT)
-    refs = {line.rstrip().split(",")[1] for file in files for index,line in enumerate(open(file)) if index>0}
     sample_to_file = {basename(file).replace("_total_pi_est.csv",""):file for file in files}
-    sorted_refs = sorted(refs)
-    sorted_samples = sorted(sample_to_file)
 
     # build a matrix
     mat = np.zeros((len(files),len(sorted_refs)))
@@ -29,6 +45,31 @@ def unfiltered_proportions(ROOT):
                 mat[index,sorted_refs.index(ref)]=float(percent)
     return mat,sorted_refs,sorted_samples
 
+def get_coord(BED_FILE,amp_list):
+    primer_to_coord = defaultdict(lambda:[[],[]])
+    amp_to_coord = {}
+    with open(BED_FILE) as bed_h:
+        for line in bed_h:
+            ref,start,end,name,_,strand = line.rstrip().split('\t')
+            if "alt" in name:
+                continue
+            amp = name.split("_LEFT")[0].split("_RIGHT")[0]
+            if strand == "+":
+                primer_to_coord[amp][0] = [int(start), int(end), strand, name]
+            if strand == "-":
+                primer_to_coord[amp][1] = [int(start), int(end), strand, name]
+    amp_to_coord = {key.split("_")[1]:[fwd[1],rev[0]] for key,(fwd,rev) in primer_to_coord.items()}
+    ranges = [amp_to_coord[amp] for amp in amp_list]
+    return list(union(ranges))
+
+def degeneracy(mat,variants,sorted_sample):
+    degen_summary = {(line.rstrip().split("\t")[0],line.rstrip().split("\t")[1]):line.rstrip().split("\t")[2].split("|----|") for index,line in enumerate(open("%s/results/degen_summary.tsv"%ROOT)) if index>0} 
+    degen_summary = {(sample):[el for el in val if el!=rep] for (sample,rep),val in degen_summary.items()}
+    variants_index = {var:index for index,var in enumerate(variants)}
+    for index,sample in enumerate(sorted_sample):
+        for variant in degen_summary[(sample)]:
+            mat[index,variants_index[variant]]=0
+    return mat
 
 
 def load_matrix(file,sep="\t",sample_order=None,strain_order=None) :
@@ -49,7 +90,7 @@ def load_matrix(file,sep="\t",sample_order=None,strain_order=None) :
         return matrix,header,strains
 
 
-def get_pos_base_percent(ROOT,prprt,RANGE):
+def get_pos_base_percent(ROOT,prprt):
     # get snv 
     posbase_percent = defaultdict(lambda:np.zeros((len(sorted_sample))))
     for line in open("%s/refs/db_snv.tsv"%ROOT):
@@ -63,11 +104,10 @@ def get_pos_base_percent(ROOT,prprt,RANGE):
         if sum(prprt[variants.index(variant),:])==0:
             continue
         pos = int(pos)
-        if RANGE[0]<=pos <= RANGE[1]:
-            posbase_percent[(pos,base)]+=prprt[variants.index(variant),:]
+        posbase_percent[(pos,base)]+=prprt[variants.index(variant),:]
     return posbase_percent
 
-def pos_base_map(ROOT,RANGE):
+def pos_base_map(ROOT):
     # get snv 
     snv_variants = defaultdict(list)
     for line in open("%s/refs/db_snv.tsv"%ROOT):
@@ -77,13 +117,14 @@ def pos_base_map(ROOT,RANGE):
             continue
         variant = variant.split(" ")[0]
         pos = int(pos)
-        if RANGE[0]<=pos <= RANGE[1]:
-            snv_variants[(pos,base)].append(variant)
+        snv_variants[(pos,base)].append(variant)
     snv_variants = {key:"|sep|".join(val) for key,val in snv_variants.items()}
     return snv_variants
 
 
-def get_observed_snv(ROOT,sorted_sample,RANGE):
+def get_observed_snv(ROOT,sorted_sample,varscan=True):
+    if varscan:
+        sample_to_pos = get_varscan_cnts(ROOT)
     cnts_matrix,sample_bases,ref = load_matrix("%s/snv/count.csv"%ROOT,sep=",")
     sample_to_indexes = defaultdict(list)
     for index,sample in enumerate(sample_bases[1:]):
@@ -96,29 +137,58 @@ def get_observed_snv(ROOT,sorted_sample,RANGE):
 
     for index,line in enumerate(cnts_matrix):
         pos = int(line[0])-1
-        if RANGE[0]<=pos<=RANGE[1]:
-            line = line[1:]
-            if sum(line)==0:
-                continue
-            for sample,indexes in sample_to_indexes.items():
-                tot_nuc = sum(line[indexes])
-                pos_cnts[pos][sorted_sample.index(sample)] = int(tot_nuc)
-                if tot_nuc!=0:
-                    for index,cnts in enumerate(line[indexes]):
-                        base = sorted_bases[index]
-                        if refseq[pos]!=base:
-                            posbase_cnts_observed[(pos,base)][sorted_sample.index(sample)] = int(cnts)
+        line = line[1:]
+        if sum(line)==0:
+            continue
+        for sample,indexes in sample_to_indexes.items():
+            if varscan:
+                if pos not in sample_to_pos[sample]:
+                    continue
+            tot_nuc = sum(line[indexes])
+            pos_cnts[pos][sorted_sample.index(sample)] = int(tot_nuc)
+            if tot_nuc!=0:
+                for index,cnts in enumerate(line[indexes]):
+                    base = sorted_bases[index]
+                    if refseq[pos]!=base:
+                        posbase_cnts_observed[(pos,base)][sorted_sample.index(sample)] = int(cnts)
     return posbase_cnts_observed,pos_cnts
 
+def filter_combi(sample_combi,ranges):
+    sorted_combi_init = sorted(sample_combi,key=lambda x:x[0])
+    sorted_combi = []
+    index_ranges = 0
+    for pos,nuc in sorted_combi_init:
+        if pos < ranges[index_ranges][0]:
+            continue
+        if pos > ranges[index_ranges][1]:
+            if index_ranges<(len(ranges)+1):
+                index_ranges+=1
+            continue
+        sorted_combi.append((pos,nuc))
+    return sorted_combi
 
-def sample_wise_data(sample_index,posbase_cnts,posbase_cnts_observed,posbase_filt, sorted_sample, snv_variants, pos_cnts):
+def get_varscan_cnts(ROOT):
+    sample_to_pos = {}
+    files = glob.glob("%s/samples/*/varscan/*_snv.tsv"%ROOT)
+    for file in files:
+        sample = basename(file).replace("_snv.tsv","")
+        pos = {int(line.rstrip().split("\t")[1])-1 for index,line in enumerate(open(file)) if index>0}
+        sample_to_pos[sample]=pos
+    return sample_to_pos
+
+
+
+
+
+def sample_wise_data(sample_index,posbase_cnts,posbase_cnts_observed,posbase_filt, sorted_sample, snv_variants, pos_cnts,ranges):
     sorted_cases = ["observed and predicted (%s)", "not predicted (%s)", "not observed (%s)", "observed only once (%s)", "removed by threshold (%s)"]
     sample_pred = {key for key,val in posbase_cnts.items() if val[sample_index]!=0}
     sample_obs = {key for key,val in posbase_cnts_observed.items() if val[sample_index]!=0}
     sample_combi = sample_pred|sample_obs
     sample_common = sample_pred&sample_obs
     color_label = {}
-    for combi in sorted(sample_combi):
+    sorted_combi = filter_combi(sample_combi,ranges)
+    for combi in sorted_combi:
         if combi in sample_common:
             color_label[combi] = "observed and predicted (%s)" 
         if combi in sample_obs-sample_pred:
@@ -135,13 +205,13 @@ def sample_wise_data(sample_index,posbase_cnts,posbase_cnts_observed,posbase_fil
                 color_label[combi] = "removed by threshold (%s)"
 
     label_to_val = {case:index for index,case in enumerate(sorted_cases)}
-    color = [label_to_val[color_label[combi]] for combi in sorted(sample_combi)]
+    color = [label_to_val[color_label[combi]] for combi in sorted_combi]
     nb = Counter(color_label.values())
     sample_cases = [case%nb[case] for case in sorted_cases]
 
     # get x,y
-    x_predict = [posbase_cnts[comb][sample_index] if comb in posbase_cnts else 0 for comb in sorted(sample_combi)]
-    y_obsv = [posbase_cnts_observed[comb][sample_index] if comb in posbase_cnts_observed else 0 for comb in sorted(sample_combi)]
+    x_predict = [posbase_cnts[comb][sample_index] if comb in posbase_cnts else 0 for comb in sorted_combi]
+    y_obsv = [posbase_cnts_observed[comb][sample_index] if comb in posbase_cnts_observed else 0 for comb in sorted_combi]
 
     # hack: not all colors are shown if I don't do that;
     x_predict += [1e-10,1e-10,1e-10,1e-10,1e-10]
@@ -150,7 +220,7 @@ def sample_wise_data(sample_index,posbase_cnts,posbase_cnts_observed,posbase_fil
 
     # create data 
     data = []
-    for combi in sorted(sample_combi):
+    for combi in sorted_combi:
         pos,base = combi
         if (combi in snv_variants)&(combi in posbase_filt):
             origin = snv_variants[combi]
@@ -179,27 +249,36 @@ name,refseq = next(sfp(open(ref_file)))
 
 # get filtered predicted variant proportions
 prprt_filt,variants,sorted_sample = load_matrix('%s/results/proportions.tsv'%ROOT)
+prprt_filt = degeneracy(prprt_filt,variants,sorted_sample)
 prprt_filt = prprt_filt.T
 
 # get unfiltered variant proportions
-prprt,variants,sorted_sample = unfiltered_proportions(ROOT)
+prprt,variants,sorted_sample = unfiltered_proportions(ROOT,variants,sorted_sample)
+prprt = degeneracy(prprt,variants,sorted_sample)
 prprt = prprt.T
 
 
 # get primer range
-primer_file = glob.glob("%s/refs/*_primer.bed"%ROOT)[0]
-RANGE = [min([int(line.rstrip().split("\t")[1]) for line in open(primer_file) if "LEFT" in line if "alt" not in line]),max([int(line.rstrip().split("\t")[1]) for line in open(primer_file) if "LEFT" not in line if "alt" not in line])]
+PRIMER_FILE = glob.glob("%s/refs/*_primer.bed"%ROOT)[0]
+RANGE = [min([int(line.rstrip().split("\t")[1]) for line in open(PRIMER_FILE) if "LEFT" in line if "alt" not in line]),max([int(line.rstrip().split("\t")[1]) for line in open(PRIMER_FILE) if "LEFT" not in line if "alt" not in line])]
+
+# for each sample assess region used for calling variants
+sample_to_range = {}
+for file in glob.glob("%s/samples/*/EM_runs/*_total_Filt_amp_pi_est.csv"%ROOT):
+    sample = basename(file).replace("_total_Filt_amp_pi_est.csv","")
+    amp_list = next(open(file)).rstrip().split(",")[2:]
+    sample_to_range[sample]=get_coord(PRIMER_FILE,amp_list)
 
 # get db snv mapping
-snv_variants = pos_base_map(ROOT,RANGE)
+snv_variants = pos_base_map(ROOT)
 
 
 # get snv proportions
-posbase_filt = get_pos_base_percent(ROOT,prprt_filt,RANGE)
-posbase_percent = get_pos_base_percent(ROOT,prprt,RANGE)
+posbase_filt = get_pos_base_percent(ROOT,prprt_filt)
+posbase_percent = get_pos_base_percent(ROOT,prprt)
 
 # get observed counts
-posbase_cnts_observed,pos_cnts = get_observed_snv(ROOT,sorted_sample,RANGE)
+posbase_cnts_observed,pos_cnts = get_observed_snv(ROOT,sorted_sample)
 
 issue = {combi for combi,values in  posbase_cnts_observed.items() if sum(pos_cnts[combi[0]]-values)>0}
 
@@ -222,7 +301,7 @@ for index_page in range(0,len(sorted_sample),3):
         plt.subplot(plot_num)
 
         # get data
-        sample_cases, color, x_predict, y_obsv, data = sample_wise_data(index_tot,posbase_cnts,posbase_cnts_observed,posbase_filt,sorted_sample,snv_variants,pos_cnts)
+        sample_cases, color, x_predict, y_obsv, data = sample_wise_data(index_tot,posbase_cnts,posbase_cnts_observed,posbase_filt,sorted_sample,snv_variants,pos_cnts,sample_to_range[sorted_sample[index_tot]])
 
         # add to result for futur tabular output
         results+=data
